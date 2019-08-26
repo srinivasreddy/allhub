831652c482520c6dde533394ee4a9714f5245f00
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-08-26 15:00:32 +0530

    Refactor the code

--- allhub/repos.py
+++ allhub/repos.py
@@ -1,35 +1,34 @@
 import requests
 import multiprocessing
-from multiprocessing import Process, Pool
+from multiprocessing import Pool
 import subprocess
-
-import os
-
-
-user_name = os.environ.get("USER_NAME", "srinivasreddy")
-per_page = os.environ.get("PER_PAGE")
-auth_token = os.environ.get("AUTH_TOKEN")
-
-url = "https://api.github.com/users/{user}/repos"
-
-if per_page:
-    url = url+"?per_page={per_page}"
 
 
 def distribute_work(_url):
     command = f"git clone {_url}"
-    subprocess.call(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    subprocess.call(
+        command,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True,
+    )
 
 
 class Repos:
-    def __init__(self, auth_token, user_name, per_page):
+    def __init__(self, auth_token, user_name, per_page=None):
         self.auth_token = auth_token
         self.user_name = user_name
         self.per_page = per_page
+        self.clone_url = "https://api.github.com/users/{user}/repos"
+        if self.per_page:
+            self.clone_url = self.clone_url + "?per_page={per_page}"
 
     def clone(self):
         list_of_repos = requests.get(
-            url.format(url, user=self.user_name, per_page=self.per_page),
+            self.clone_url.format(
+                self.clone_url, user=self.user_name, per_page=self.per_page
+            ),
             headers={"Authorization": self.auth_token},
         ).json()
         urls = [repo["clone_url"] for repo in list_of_repos]
