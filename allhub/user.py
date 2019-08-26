09bd288eebccbae6d96b1bb766bb934d1139413b
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-08-26 15:40:44 +0530

    Add files.

--- allhub/user.py
+++ allhub/user.py
@@ -2,9 +2,21 @@ import requests
 import multiprocessing
 from multiprocessing import Pool
 import subprocess
+from distutils.spawn import find_executable
+import sys
 
 
-def distribute_work(_url):
+def check_git_installed():
+    # TODO: move to utils.
+    git_location = find_executable("git")
+    if git_location is None:
+        sys.stderr.write(
+            "git is not installed. " "Please install git for your operating system.\n"
+        )
+        sys.exit(-1)
+
+
+def shell_git_clone(_url):
     # TODO: How to recover from the network failure, existing repos??
     command = f"git clone {_url}"
     subprocess.call(
@@ -28,16 +40,30 @@ class User:
 
     @property
     def clone_urls(self):
+        check_git_installed()  # TODO: Move this call to decorator.
         list_of_repos = requests.get(
             self.clone_url.format(user=self.user_name),
             headers={"Authorization": self.auth_token},
         ).json()
         return [repo["clone_url"] for repo in list_of_repos]
 
-    def clone(self, urls=None):
+    def _build_clone_url(self, repo_name):
+        return f"https://github.com/{self.user_name}/{repo_name}.git"
+
+    def clone_repos(self, urls=None):
+        check_git_installed()
         if urls is None:
             urls = self.clone_urls
         if not isinstance(urls, (tuple, list)):
             raise AttributeError("urls should be list or tuple")
         with Pool(processes=multiprocessing.cpu_count()) as p:
-            p.map(distribute_work, urls)
+            p.map(shell_git_clone, urls)
+
+    def clone_repo(self, repo_name):
+        check_git_installed()
+        if not isinstance(repo_name, str):
+            raise AttributeError(
+                f"repo_name should be string, instead got {type(repo_name)}"
+            )
+        clone_url = self._build_clone_url(repo_name)
+        shell_git_clone(clone_url)
