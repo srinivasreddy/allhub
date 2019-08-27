6be7c0b893c2e0e788a483b504bb446a0c8d86ff
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-08-27 18:32:53 +0530

    Add methods of gist

--- allhub/gist.py
+++ allhub/gist.py
@@ -1,8 +1,24 @@
 class Gist:
     def gist_comments(self, gist_id):
         url = f"https://api.github.com/users/gists/{gist_id}/comments"
-        return self.partial(url).json()
+        return self.get_partial(url).json()
 
     def gist_comment(self, gist_id, comment_id):
         url = f"https://api.github.com/users/gists/{gist_id}/comments/{comment_id}"
-        return self.partial(url).json()
+        return self.get_partial(url).json()
+
+    def list_gists(self):
+        url = f"https://api.github.com/users/{self.user_name}/gists"
+        return self.get_partial(url).json()
+
+    def list_public_gists(self):
+        url = f"https://api.github.com/users/gists/public"
+        return self.get_partial(url).json()
+
+    def list_starred_gists(self):
+        url = f"https://api.github.com/users/gists/public"
+        return self.get_partial(url).json()
+
+    def gist(self, gist_id):
+        url = f"https://api.github.com/users/gists/{gist_id}"
+        return self.get_partial(url).json()
