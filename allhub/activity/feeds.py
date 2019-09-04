2427d90d94a8fe86df703e5e01d0eae98d1c58e5
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-09-04 17:32:26 +0530

    Add methods to FeedMixin

--- allhub/activity/feeds.py
+++ allhub/activity/feeds.py
@@ -1,2 +1,18 @@
+from allhub.response import Response
+
+
 class FeedsMixin:
-    pass
+    def feeds(self):
+        """
+        List all the feeds in a JSON response.
+        """
+        url = "/feeds"
+        self.response = Response(self.get(url), "Feeds")
+        return self.response.transform()
+
+    def security_advisory_feed(self):
+        url = self.feeds()["security-advisories"]
+        self.response = Response(
+            self.get(url, Accept="application/atom+xml"), "SecurityAdvFeed"
+        )
+        return self.response.content()
