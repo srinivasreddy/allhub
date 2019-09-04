dbeef124835f0b4518b888465235fae6ab0a51c2
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-09-04 17:53:26 +0530

    Add various feed to the FeedsMixin

--- allhub/activity/feeds.py
+++ allhub/activity/feeds.py
@@ -2,9 +2,16 @@ from allhub.response import Response
 
 
 class FeedsMixin:
+    """
+    TODO:
+    security_advisory_feed is only the current feed added here.
+    Please feel free to add them later on when required.
+    """
+
     def feeds(self):
         """
-        List all the feeds in a JSON response.
+        List all the feeds in a JSON response.Though we use plural here response
+        is a dictionary not list.
         """
         url = "/feeds"
         self.response = Response(self.get(url), "Feeds")
@@ -14,5 +21,30 @@ class FeedsMixin:
         url = self.feeds()["security_advisories_url"]
         self.response = Response(
             self.get(url, Accept="application/atom+xml"), "SecurityAdvFeed"
+        )
+        return self.response.content()
+
+    def timeline_feed(self):
+        url = self.feeds()._links.timeline.href
+        self.response = Response(
+            self.get(url, Accept="application/atom+xml"), "TimelineFeed"
+        )
+        return self.response.content()
+
+    def user_feed(self, username):
+        url = self.feeds()._links.user.href.format(user=username)
+        self.response = Response(
+            self.get(url, Accept="application/atom+xml"), "UserFeed"
+        )
+        return self.response.content()
+
+    def current_user_feed(self):
+        """
+        This is feed is gives only public data.
+        :return:
+        """
+        url = self.feeds()._links.current_user_public.href
+        self.response = Response(
+            self.get(url, Accept="application/atom+xml"), "CurrentUserFeed"
         )
         return self.response.content()
