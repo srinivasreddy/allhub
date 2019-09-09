edae2871c387fbc9bffcd729ac98cc5de884a647
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-09-09 14:04:06 +0530

    Add block/unblock/methods in user_mixin.py

--- allhub/user_mixin.py
+++ allhub/user_mixin.py
@@ -1,10 +1,57 @@
-from .transform import transform
+from .response import Response
 
 
 class UserMixin:
     def list_blocked_users(self):
         url = f"/user/blocks"
-        return transform("BlockedUsers", self.get_basic(url))
+        self.response = Response(
+            self.get(
+                url,
+                **{"Accept": "application/vnd.github.giant-sentry-fist-preview+json"},
+            ),
+            "BlockedUsers",
+        )
+        return self.response.transform()
 
     def endpoints(self):
-        return transform("Endpoints", self.get(""))
+        self.response = Response(self.get(""), "Endpoints")
+        return self.response.transform()
+
+    def blocked(self, username):
+        """
+        Check whether you've blocked a user
+        """
+        url = f"/user/blocks/{username}"
+        self.response = Response(self.get(url), "")
+        if self.response.status_code == 204:
+            return True
+        elif self.response.status_code == 404:
+            return False
+        else:
+            return None
+
+    def block(self, username):
+        """
+        Block a user
+        """
+        url = f"/user/blocks/{username}"
+        self.response = Response(self.put(url), "")
+        if self.response.status_code == 204:
+            return True
+        elif self.response.status_code == 404:
+            return False
+        else:
+            return None
+
+    def unblock(self, username):
+        """
+        Unblock a user
+        """
+        url = f"/user/blocks/{username}"
+        self.response = Response(self.delete(url), "")
+        if self.response.status_code == 204:
+            return True
+        elif self.response.status_code == 404:
+            return False
+        else:
+            return None
