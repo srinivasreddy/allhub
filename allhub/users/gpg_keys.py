c19ac07aef12a283e88b9bf2b1676cf28ba9ed32
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-09-09 16:56:54 +0530

    Add urls for GPGKeys

---
+++ allhub/users/gpg_keys.py
@@ -0,0 +1,59 @@
+from allhub.response import Response
+
+
+class GPGKeysMixin:
+    def list_gpg_keys(self, username):
+        url = f"/users/{username}/gpg_keys"
+        self.response = Response(
+            self.get(
+                url,
+                **{"Accept": "application/vnd.github.giant-sentry-fist-preview+json"},
+            ),
+            "GPGKeys",
+        )
+        return self.response.transform()
+
+    def gpg_keys(self):
+        url = f"/users/gpg_keys"
+        self.response = Response(
+            self.get(
+                url,
+                **{"Accept": "application/vnd.github.giant-sentry-fist-preview+json"},
+            ),
+            "GPGKeys",
+        )
+        return self.response.transform()
+
+    def gpg_key(self, gpg_key_id):
+        url = f"/user/gpg_keys/{gpg_key_id}"
+        self.response = Response(
+            self.get(
+                url,
+                **{"Accept": "application/vnd.github.giant-sentry-fist-preview+json"},
+            ),
+            "GPGKey",
+        )
+        return self.response.transform()
+
+    def create_public_key(self, armored_public_key):
+        url = "/user/gpg_keys"
+        self.response = Response(
+            self.post(
+                url,
+                json=[("armored_public_key", armored_public_key)]
+                ** {"Accept": "application/vnd.github.giant-sentry-fist-preview+json"},
+            ),
+            "SSHKey",
+        )
+        return self.response.transform()
+
+    def delete_public_key(self, gpg_key_id):
+        url = f"/user/gpg_keys/{gpg_key_id}"
+        self.response = Response(
+            self.delete(
+                url,
+                **{"Accept": "application/vnd.github.giant-sentry-fist-preview+json"},
+            ),
+            "",
+        )
+        return self.response.status_code == 204
