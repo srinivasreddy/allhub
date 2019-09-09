6203faac85383e4c2310af1b4ab9189722fe064b
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-09-09 14:47:59 +0530

    Add emails.py files with some more methods.

--- allhub/users/emails.py
+++ allhub/users/emails.py
@@ -44,3 +44,22 @@ class EmailMixin:
             "PublicEmails",
         )
         return self.response.transform()
+
+    def delete_email(self, emails):
+        url = "/user/emails"
+        if isinstance(emails, (list, tuple)):
+            for email in emails:
+                if not isinstance(email, str):
+                    raise ValueError("email should be string")
+        elif not isinstance(emails, str):
+            raise ValueError("The email should either be a string or list of strings")
+
+        self.response = Response(
+            self.delete(
+                url,
+                payload=[("emails", emails)],
+                **{"Accept": "application/vnd.github.giant-sentry-fist-preview+json"},
+            ),
+            "",
+        )
+        return self.response.status_code == 204
