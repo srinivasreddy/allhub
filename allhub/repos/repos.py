66d1da587d782ddc7c257c562e9b5fcddb59bf26
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-09-10 15:06:36 +0530

    Add some more methods.

--- allhub/repos/repos.py
+++ allhub/repos/repos.py
@@ -43,6 +43,10 @@ class ReposMixin:
         """
         Lists repositories that the authenticated user has explicit permission
         (:read, :write, or :admin) to access.
+
+        The authenticated user has explicit permission to access repositories they own,
+        repositories where they are a collaborator, and repositories that they can access
+        through an organization membership.
         """
         params = []
         if "visibility" in kwargs:
@@ -54,7 +58,6 @@ class ReposMixin:
                     f"'{visibility}' should either be 'ALL','PUBLIC', or 'PRIVATE'"
                 )
             params.append(("visibility", visibility.value))
-
         if "affiliation" in kwargs:
             affiliation = kwargs.pop("affiliation")
             for aff in affiliation.split(","):
@@ -63,12 +66,10 @@ class ReposMixin:
                         f"'{aff}' should either be 'owner', 'collaborator', or 'organization_member'"
                     )
             params.append(("affiliation", affiliation))
-
         if "type" in kwargs and ("visibility" in kwargs or "affiliation" in kwargs):
             raise AttributeError(
                 "If you specify visibility or affiliation, you cannot specify type."
             )
-
         if "type" in kwargs:
             type = kwargs.pop("type")
             if not isinstance(type, Type):
@@ -78,7 +79,6 @@ class ReposMixin:
                     f"'{type}' should either be 'ALL', 'OWNER', 'PUBLIC', 'PRIVATE', or 'MEMBER'"
                 )
             params.append(("type", type.value))
-
         if "sort" in kwargs:
             sort = kwargs.pop("sort")
             if not isinstance(sort, Sort):
@@ -88,7 +88,6 @@ class ReposMixin:
                     f"'{sort}' should either be 'CREATED', 'UPDATED', 'PUSHED', or 'FULLNAME'"
                 )
             params.append(("sort", sort.value))
-
         if "direction" in kwargs:
             direction = kwargs.pop("direction")
             if not isinstance(direction, Direction):
@@ -96,7 +95,77 @@ class ReposMixin:
             if direction not in Direction:
                 raise ValueError(f"'{direction}' should either be 'ASC', or 'DESC'")
             params.append(("direction", direction.value))
-
         url = "/user/repos"
+        self.response = Response(self.get_basic(url, params=params), "Repos")
+        return self.response.transform()
+
+    def user_repos(self, username, **kwargs):
+        params = []
+        if "type" in kwargs:
+            type = kwargs.pop("type")
+            if not isinstance(type, Type):
+                raise ValueError(f"'{type}' should be instance of Type")
+            if type not in Type:
+                raise ValueError(
+                    f"'{type}' should either be 'ALL', 'OWNER', 'PUBLIC', 'PRIVATE', or 'MEMBER'"
+                )
+            params.append(("type", type.value))
+        if "sort" in kwargs:
+            sort = kwargs.pop("sort")
+            if not isinstance(sort, Sort):
+                raise ValueError(f"{sort} should be instance of Sort")
+            if sort not in Sort:
+                raise ValueError(
+                    f"'{sort}' should either be 'CREATED', 'UPDATED', 'PUSHED', or 'FULLNAME'"
+                )
+            params.append(("sort", sort.value))
+        if "direction" in kwargs:
+            direction = kwargs.pop("direction")
+            if not isinstance(direction, Direction):
+                raise ValueError(f"'{direction}' is not an instance of Direction")
+            if direction not in Direction:
+                raise ValueError(f"'{direction}' should either be 'ASC', or 'DESC'")
+            params.append(("direction", direction.value))
+        url = f"/users/{username}/repos"
+        self.response = Response(self.get_basic(url, params=params), "Repos")
+        return self.response.transform()
+
+    def org_repos(self, org, **kwargs):
+        params = []
+        if "type" in kwargs:
+            type = kwargs.pop("type")
+            if not isinstance(type, Type):
+                raise ValueError(f"'{type}' should be instance of Type")
+            if type not in Type:
+                raise ValueError(
+                    f"'{type}' should either be 'ALL', 'OWNER', 'PUBLIC', 'PRIVATE', or 'MEMBER'"
+                )
+            params.append(("type", type.value))
+        if "sort" in kwargs:
+            sort = kwargs.pop("sort")
+            if not isinstance(sort, Sort):
+                raise ValueError(f"{sort} should be instance of Sort")
+            if sort not in Sort:
+                raise ValueError(
+                    f"'{sort}' should either be 'CREATED', 'UPDATED', 'PUSHED', or 'FULLNAME'"
+                )
+            params.append(("sort", sort.value))
+        if "direction" in kwargs:
+            direction = kwargs.pop("direction")
+            if not isinstance(direction, Direction):
+                raise ValueError(f"'{direction}' is not an instance of Direction")
+            if direction not in Direction:
+                raise ValueError(f"'{direction}' should either be 'ASC', or 'DESC'")
+            params.append(("direction", direction.value))
+        url = f"/users/{org}/repos"
+        self.response = Response(self.get_basic(url, params=params), "Repos")
+        return self.response.transform()
+
+    def all_repos(self, **kwargs):
+        url = "/repositories"
+        params = []
+        if "since" in kwargs:
+            since = kwargs.pop("since")
+            params.append(("since", since))
         self.response = Response(self.get_basic(url, params=params), "Repos")
         return self.response.transform()
