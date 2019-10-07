8903009a894709ce97c5dcda25bb117e3d9f21e7
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-07 17:34:31 +0530

    Add release API

--- allhub/repos/releases.py
+++ allhub/repos/releases.py
@@ -9,17 +9,17 @@ class ReleaseMixin:
 
     def release(self, owner, repo, release_id):
         url = f"/repos/{owner}/{repo}/releases/{release_id}"
-        self.response = Response(self.get(url), "Releases")
+        self.response = Response(self.get(url), "Release")
         return self.response.transform()
 
     def latest_release(self, owner, repo):
         url = f"/repos/{owner}/{repo}/releases/latest"
-        self.response = Response(self.get(url), "Releases")
+        self.response = Response(self.get(url), "Release")
         return self.response.transform()
 
     def latest_release_by_tag_name(self, owner, repo, tag):
         url = f"/repos/{owner}/{repo}/releases/tags/{tag}"
-        self.response = Response(self.get(url), "Releases")
+        self.response = Response(self.get(url), "Release")
         return self.response.transform()
 
     def create_release(
@@ -71,5 +71,28 @@ class ReleaseMixin:
 
     def delete_release(self, owner, repo, release_id):
         url = f"/repos/{owner}/{repo}/releases/{release_id}"
+        self.response = Response(self.delete(url), "")
+        return self.response.status_code == 204
+
+    def release_assets(self, owner, repo, release_id):
+        url = f"/repos/{owner}/{repo}/releases/{release_id}/assets"
+        self.response = Response(self.get(url), "ReleaseAssets")
+        return self.response.transform()
+
+    # TODO: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+
+    def release_asset(self, owner, repo, asset_id):
+        url = f"/repos/{owner}/{repo}/releases/assets/{asset_id}"
+        self.response = Response(self.get(url), "ReleaseAsset")
+        return self.response.transform()
+
+    def edit_release_asset(self, owner, repo, asset_id, name, label):
+        params = {"name": name, "label": label}
+        url = f"/repos/{owner}/{repo}/releases/assets/{asset_id}"
+        self.response = Response(self.patch(url, params=params), "ReleaseAsset")
+        return self.response.transform()
+
+    def delete_release_asset(self, owner, repo, asset_id):
+        url = f"/repos/{owner}/{repo}/releases/assets/{asset_id}"
         self.response = Response(self.delete(url), "")
         return self.response.status_code == 204
