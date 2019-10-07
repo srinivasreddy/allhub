c5f401a14b1082cb1ec957431715001c1f2926e6
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-07 12:06:04 +0530

    Update the repo method names.:

--- allhub/repos/webhooks.py
+++ allhub/repos/webhooks.py
@@ -2,17 +2,17 @@ from allhub.response import Response
 
 
 class WebHooksMixin:
-    def hooks(self, owner, repo):
+    def repo_hooks(self, owner, repo):
         url = f"/repos/{owner}/{repo}/hooks"
         self.response = Response(self.get(url), "Hooks")
         return self.response.transform()
 
-    def hook(self, owner, repo, hook_id):
+    def repo_hook(self, owner, repo, hook_id):
         url = f"/repos/{owner}/{repo}/hooks/{hook_id}"
         self.response = Response(self.get(url), "Hook")
         return self.response.transform()
 
-    def create_hook(self, owner, repo, config, events, name="web", active=True):
+    def create_repo_hook(self, owner, repo, config, events, name="web", active=True):
         url = f"/repos/{owner}/{repo}/hooks"
         assert isinstance(config, dict)
         assert isinstance(events, (list, tuple))
@@ -20,7 +20,7 @@ class WebHooksMixin:
         self.response = Response(self.post(url, params=params), "Hook")
         return self.response.transform()
 
-    def edit_hook(
+    def edit_repo_hook(
         self,
         owner,
         repo,
@@ -44,7 +44,7 @@ class WebHooksMixin:
         self.response = Response(self.patch(url, params=params), "Hook")
         return self.response.transform()
 
-    def test_push_hook(self, owner, repo, hook_id):
+    def test_repo_push_hook(self, owner, repo, hook_id):
         url = f"/repos/{owner}/{repo}/hooks/{hook_id}/tests"
         self.response = Response(self.post(url), "")
         if self.response.status_code == 204:
@@ -53,7 +53,7 @@ class WebHooksMixin:
             f"test_push_hook(.....) returned {self.response.status_code}, instead it should return 204."
         )
 
-    def ping_hook(self, owner, repo, hook_id):
+    def ping_repo_hook(self, owner, repo, hook_id):
         url = f"/repos/{owner}/{repo}/hooks/{hook_id}/pings"
         self.response = Response(self.post(url), "")
         if self.response.status_code == 204:
@@ -62,7 +62,7 @@ class WebHooksMixin:
             f"ping_hook(.....) returned {self.response.status_code}, instead it should return 204."
         )
 
-    def delete_hook(self, owner, repo, hook_id):
+    def delete_repo_hook(self, owner, repo, hook_id):
         url = f"/repos/{owner}/{repo}/hooks/{hook_id}"
         self.response = Response(self.delete(url), "")
         if self.response.status_code == 204:
