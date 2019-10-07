15ce0f8947e9cc2547463bc67614f4c030d1fd56
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-07 12:42:42 +0530

    Update branch protection

--- allhub/repos/branches.py
+++ allhub/repos/branches.py
@@ -20,3 +20,26 @@ class BranchMixin:
         url = f"/repos/{owner}/{repo}/branches/{branch}/protection"
         self.response = Response(self.get(url, **{"Accept": mime}), "BranchProtection")
         return self.response.transform()
+
+    def update_branch_protection(
+        self,
+        owner,
+        repo,
+        branch,
+        required_status_checks,
+        enforce_admins,
+        required_pull_request_reviews,
+        restrictions,
+    ):
+        mime = "application/vnd.github.luke-cage-preview+json"
+        url = f"/repos/{owner}/{repo}/branches/{branch}/protection"
+        params = {
+            "required_status_checks": required_status_checks,
+            "enforce_admins": enforce_admins,
+            "required_pull_request_reviews": required_pull_request_reviews,
+            "restrictions": restrictions,
+        }
+        self.response = Response(
+            self.put(url, params=params, **{"Accept": mime}), "UpdateBranchProtection"
+        )
+        return self.response.transform()
