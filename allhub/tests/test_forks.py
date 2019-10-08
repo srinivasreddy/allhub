f5cef66f02e6c6308d2505596b2a172e411202e2
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-08 18:03:37 +0530

    Add tests for forks

--- allhub/tests/test_forks.py
+++ allhub/tests/test_forks.py
@@ -8,10 +8,15 @@ class TestForks:
         assert response.owner.login == "test-github42"
 
     def test_forks(self):
-        response = allhub.forks("srinivasreddy", "allhub")
-        data = [
-            fork
-            for fork in response
-            if fork.owner.login == "test-github42" and fork.name == "allhub"
-        ]
-        assert len(data) == 1
+        response = allhub.create_fork("python", "cpython")
+        for response in allhub.iterator(allhub.forks, "python", "cpython"):
+            data = [
+                fork
+                for fork in response
+                if fork.owner.login == "test-github42" and fork.name == "cpython"
+            ]
+            if data:
+                assert len(data) == 1
+                break
+        else:
+            assert False, 'The repo "python/cpython" should have been forked.'
