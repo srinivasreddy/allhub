4f78ed3920ce708773ff236026602053456cbdcf
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-08 17:11:21 +0530

    Update forks API

--- allhub/repos/forks.py
+++ allhub/repos/forks.py
@@ -9,16 +9,18 @@ class Sort(Enum):
 
 
 class ForkMixin:
-    def forks(self, owner, repo, sort=Sort.NEWEST):
+    def forks(self, owner, repo, sort=Sort.NEWEST, **kwargs):
         url = f"/repos/{owner}/{repo}/forks"
-        self.response = Response(self.get(url, params=[("sort", sort.value)]), "Forks")
+        self.response = Response(
+            self.get(url, params=[("sort", sort.value)], **kwargs), "Forks"
+        )
         return self.response.transform()
 
-    def create_fork(self, owner, repo, organization=None):
+    def create_fork(self, owner, repo, organization=None, **kwargs):
         url = f"/repos/{owner}/{repo}/forks"
         params = {}
         if organization:
             params["organization"] = organization
-        self.response = Response(self.post(url, params=params), "Fork")
+        self.response = Response(self.post(url, params=params, **kwargs), "Fork")
         assert self.response.status_code == 202
         return self.response.transform()
