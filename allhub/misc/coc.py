d41b6864ef443eb2be9340b5762f6e25c3d64029
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-08 19:32:28 +0530

    Refactor mime constant in coc.py

--- allhub/misc/coc.py
+++ allhub/misc/coc.py
@@ -1,43 +1,25 @@
 from allhub.response import Response
 
+_mime = "application/vnd.github.scarlet-witch-preview+json"
+
 
 class CodeOfConductMixin:
     def codes_of_conduct(self):
         url = "/codes_of_conduct"
-        self.response = Response(
-            self.get(
-                url, **{"Accept": "application/vnd.github.scarlet-witch-preview+json"}
-            ),
-            "COCs",
-        )
+        self.response = Response(self.get(url, **{"Accept": _mime}), "COCs")
         return self.response.transform()
 
     def code_of_conduct(self, key):
         url = f"/codes_of_conduct/{key}"
-        self.response = Response(
-            self.get(
-                url, **{"Accept": "application/vnd.github.scarlet-witch-preview+json"}
-            ),
-            "COC",
-        )
+        self.response = Response(self.get(url, **{"Accept": _mime}), "COC")
         return self.response.transform()
 
     def repo_code_of_conduct(self, owner, repo):
         url = f"/repos/{owner}/{repo}"
-        self.response = Response(
-            self.get(
-                url, **{"Accept": "application/vnd.github.scarlet-witch-preview+json"}
-            ),
-            "RepoCOC",
-        )
+        self.response = Response(self.get(url, **{"Accept": _mime}), "RepoCOC")
         return self.response.transform()
 
     def contents_of_repo_code_of_conduct(self, owner, repo):
         url = f"/repos/{owner}/{repo}/community/code_of_conduct"
-        self.response = Response(
-            self.get(
-                url, **{"Accept": "application/vnd.github.scarlet-witch-preview+json"}
-            ),
-            "RepoCOC",
-        )
+        self.response = Response(self.get(url, **{"Accept": _mime}), "RepoCOC")
         return self.response.transform()
