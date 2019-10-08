acf1110457ecf0240a4d8b2b628de4effe38fb5a
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-08 12:15:20 +0530

    Add tests for forking

---
+++ allhub/tests/test_forks.py
@@ -0,0 +1,17 @@
+from .utils import allhub
+
+
+class TestForks:
+    def test_create_fork(self):
+        response = allhub.create_fork("srinivasreddy", "allhub")
+        assert response.name == "allhub"
+        assert response.owner.login == "test-github42"
+
+    def test_forks(self):
+        response = allhub.forks("srinivasreddy", "allhub")
+        data = [
+            fork
+            for fork in response
+            if fork.owner.login == "test-github42" and fork.name == "allhub"
+        ]
+        assert len(data) > 0
