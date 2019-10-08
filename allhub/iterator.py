f43f1e78bdcd24f86eac3c7526bcf71e5ecb7be8
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-08 17:09:50 +0530

    Convert paginator as iterator.py

--- allhub/iterator.py
+++ allhub/iterator.py
@@ -1,18 +1,27 @@
-class Paginator:
-    def __init__(self, function, args, kwargs, per_page, page=1):
+class Iterator:
+    def __init__(self, client, function, per_page, page, *args, **kwargs):
+        self.client = client
         self.function = function
         self.args = args
         self.kwargs = kwargs
-        self.per_page = per_page
+        self.next = True
         self.page = page
+        self.per_page = per_page
+        self.kwargs.update({"per_page": per_page, "page": page})
 
-    @property
-    def page(self):
-        self.page
+    def __iter__(self):
+        return self
 
-    @property
-    def per_page(self):
-        self.per_page
+    def __next__(self):
+        if not self.next:
+            raise StopIteration()
+        results = self.page_results(self.page)
+        self.page += 1
+        if self.client.response.next_link() is None:
+            self.next = False
+        return results
 
-    def page_results(self, page):
-        return self.function(self.args, self.kwargs, self.per_page, page)
+    def page_results(self, page=1):
+        self.kwargs.update({"page": page})
+        self.response = self.function(*self.args, **self.kwargs)
+        return self.response
