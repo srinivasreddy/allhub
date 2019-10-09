90e0200888d48d8ae4702e62b7e5503cf402ad1f
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-09 13:44:29 +0530

    rename methods.

--- allhub/orgs/webhooks.py
+++ allhub/orgs/webhooks.py
@@ -2,17 +2,17 @@ from allhub.response import Response
 
 
 class WebHooksMixin:
-    def hooks(self, org):
+    def org_hooks(self, org):
         url = f"/orgs/{org}/hooks"
         self.response = Response(self.get(url), "WebHooks")
         return self.response.transform()
 
-    def hook(self, org, hook_id):
+    def org_hook(self, org, hook_id):
         url = f"/orgs/{org}/hooks/{hook_id}"
         self.response = Response(self.get(url), "WebHook")
         return self.response.transform()
 
-    def create_hook(self, org, name, config, events=("push",), active=True):
+    def create_org_hook(self, org, name, config, events=("push",), active=True):
         assert isinstance(config, dict)
         url = f"/orgs/{org}/hooks/"
         params = {
@@ -24,14 +24,14 @@ class WebHooksMixin:
         self.response = Response(self.post(url, params=params), "WebHook")
         return self.response.transform()
 
-    def edit_hook(self, org, hook_id, config, events=("push",), active=True):
+    def edit_org_hook(self, org, hook_id, config, events=("push",), active=True):
         assert isinstance(config, dict)
         url = f"/orgs/{org}/hooks/{hook_id}"
         params = {"active": active, "events": list(events), "config": config}
         self.response = Response(self.patch(url, params=params), "WebHook")
         return self.response.transform()
 
-    def ping_hook(self, org, hook_id):
+    def ping_org_hook(self, org, hook_id):
         url = f"/orgs/{org}/hooks/{hook_id}/pings"
         self.response = Response(self.post(url), "")
         if self.response.status_code == 204:
@@ -40,7 +40,7 @@ class WebHooksMixin:
             f"ping_hook(....) returned status code: {self.response.status_code}, instead of 204."
         )
 
-    def delete_hook(self, org, hook_id):
+    def delete_org_hook(self, org, hook_id):
         url = f"/orgs/{org}/hooks/{hook_id}"
         self.response = Response(self.delete(url), "")
         if self.response.status_code == 204:
