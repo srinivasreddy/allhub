143f23a02d9348bad6aff0c5c2efaf0375e822ba
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-09 11:13:55 +0530

    Add todo

--- allhub/tests/repos/test_webhooks.py
+++ allhub/tests/repos/test_webhooks.py
@@ -4,4 +4,5 @@ from allhub.tests.utils import allhub
 class TestWebHooks:
     def test_repo_hooks(self):
         response = allhub.repo_hooks("python", "cpython")
+        # TODO: is not done yet.
         assert response is not None
