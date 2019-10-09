fe288d1b89a1bc3493d30dfc9a2e74d42ee5ed8d
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-09 10:24:14 +0530

    Add meta tests

---
+++ allhub/tests/test_meta.py
@@ -0,0 +1,12 @@
+from .utils import allhub
+
+
+class TestMeta:
+    def test_meta(self):
+        response = allhub.meta()
+        # NOTE: This response includes Values are mostly None
+        # Needs to know the reason.
+        assert "verifiable_password_authentication" in response
+        assert "hooks" in response
+        assert "api" in response
+        assert "web" in response
