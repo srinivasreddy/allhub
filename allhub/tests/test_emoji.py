2ef8c1764d3b325e67dfeb8e2aa8ca058fd7de9e
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-09 09:28:10 +0530

    Add emoji tests

---
+++ allhub/tests/test_emoji.py
@@ -0,0 +1,11 @@
+from .utils import allhub
+
+
+class TestEmoji:
+    def test_emojis(self):
+        response = allhub.emojis()
+        assert response is not None
+        assert "+1" in response
+        assert "-1" in response
+        assert "wheel_of_dharma" in response
+        assert "shit" in response
