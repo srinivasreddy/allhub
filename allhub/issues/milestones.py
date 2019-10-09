ef8d73b05862e69cab1a06ca270e5fdb4ae1edff
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-09 16:57:46 +0530

    Add API for milestones

---
+++ allhub/issues/milestones.py
@@ -0,0 +1,71 @@
+from allhub.response import Response
+from enum import Enum
+
+
+class MilestoneSort(Enum):
+    DUE_ON = "due_on"
+    COMPLETENESS = "completeness"
+
+
+class MilestoneState(Enum):
+    OPEN = "open"
+    CLOSED = "closed"
+    ALL = "all"
+
+
+class MilestoneDirection(Enum):
+    ASC = "asc"
+    DSC = "DSC"
+
+
+class MilestoneMixin:
+    def milestones(
+        self,
+        owner,
+        repo,
+        state=MilestoneState.OPEN,
+        sort=MilestoneSort.DUE_ON,
+        direction=MilestoneDirection.ASC,
+    ):
+        url = f"/repos/{owner}/{repo}/milestones"
+        params = {
+            "state": state.value,
+            "sort": sort.value,
+            "direction": direction.value,
+        }
+        self.response = Response(self.get(url, params=params), "Milestones")
+        return self.response.transform()
+
+    def milestone(self, owner, repo, milestone_number):
+        url = f"/repos/{owner}/{repo}/milestones/{milestone_number}"
+        self.response = Response(self.get(url), "Milestone")
+        return self.response.transform()
+
+    def create_milestone(self, owner, repo, title, state, description, due_on):
+        url = f"/repos/{owner}/{repo}/milestones"
+        params = {
+            "title": title,
+            "state": state,
+            "description": description,
+            "due_on": due_on,
+        }
+        self.response = Response(self.post(url, params=params), "Milestone")
+        return self.response.transform()
+
+    def update_milestone(
+        self, owner, repo, milestone_number, title, state, description, due_on
+    ):
+        url = f"/repos/{owner}/{repo}/milestones/{milestone_number}"
+        params = {
+            "title": title,
+            "state": state,
+            "description": description,
+            "due_on": due_on,
+        }
+        self.response = Response(self.patch(url, param=params), "Milestone")
+        return self.response.transform()
+
+    def delete_milestone(self, owner, repo, milestone_number):
+        url = f"/repos/{owner}/{repo}/milestones/{milestone_number}"
+        self.response = Response(self.delete(url), "")
+        return self.response.status_code == 204
