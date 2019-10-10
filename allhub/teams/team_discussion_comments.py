68977901e2138ad571cf849e768b1f899b85deb3
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-10 19:05:42 +0530

    Add methods for Team discussion Comments

---
+++ allhub/teams/team_discussion_comments.py
@@ -0,0 +1,65 @@
+from allhub.response import Response
+
+_mime = ", ".join(
+    [
+        "application/vnd.github.echo-preview+json",
+        "application/vnd.github.squirrel-girl-preview",
+    ]
+)
+from enum import Enum
+
+
+class TeamDiscussionCommentDirection(Enum):
+    ASC = "asc"
+    DSC = "dsc"
+
+
+class TeamDiscussionCommentsMixin:
+    def team_discussion_comments(
+        self, team_id, discussion_number, direction=TeamDiscussionCommentDirection.DSC
+    ):
+        url = f"/teams/{team_id}/discussions/{discussion_number}/comments"
+        params = {"direction": direction.value}
+        self.response = Response(
+            self.get(url, params=params, **{"Accept": _mime}), "OrgTeam"
+        )
+        return self.response.transform()
+
+    def team_discussion_comment(
+        self,
+        team_id,
+        discussion_number,
+        comment_number,
+        direction=TeamDiscussionCommentDirection.DSC,
+    ):
+        url = f"/teams/{team_id}/discussions/{discussion_number}/comments/{comment_number}"
+        params = {"direction": direction.value}
+        self.response = Response(
+            self.get(url, params=params, **{"Accept": _mime}), "OrgTeam"
+        )
+        return self.response.transform()
+
+    def create_team_discussion_comment(self, team_id, discussion_number, body):
+        url = f"/teams/{team_id}/discussions/{discussion_number}/comments"
+        params = {"body": body}
+        self.response = Response(
+            self.post(url, params=params, **{"Accept": _mime}), "OrgTeam"
+        )
+        return self.response.transform()
+
+    def edit_team_discussion_comment(
+        self, team_id, discussion_number, comment_number, body
+    ):
+        url = f"/teams/{team_id}/discussions/{discussion_number}/comments/{comment_number}"
+        params = {"body": body}
+        self.response = Response(
+            self.patch(url, params=params, **{"Accept": _mime}), "OrgTeam"
+        )
+        return self.response.transform()
+
+    def delete_team_discussion_comment(
+        self, team_id, discussion_number, comment_number
+    ):
+        url = f"/teams/{team_id}/discussions/{discussion_number}/comments/{comment_number}"
+        self.response = Response(self.delete(url, **{"Accept": _mime}), "")
+        return self.response.status_code == 204
