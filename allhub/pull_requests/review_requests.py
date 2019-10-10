da70a05c370a449bd018def6cb389e68429098d1
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-10 16:27:37 +0530

    Add methods for review requests

---
+++ allhub/pull_requests/review_requests.py
@@ -0,0 +1,27 @@
+from allhub.response import Response
+
+
+class ReviewRequestsMixin:
+    def pr_review_requests(self, owner, repo, pull_number):
+        url = f"/repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers"
+        self.response = Response(self.get(url), "ReviewRequests")
+        return self.response.transform()
+
+    def create_pr_review_request(
+        self, owner, repo, pull_number, reviewers, team_reviewers
+    ):
+        _mime = "application/vnd.github.symmetra-preview+json"
+        url = f"/repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers"
+        params = {"reviewers": reviewers, "team_reviewers": team_reviewers}
+        self.response = Response(
+            self.post(url, params=params, **{"Accept": _mime}), "ReviewRequest"
+        )
+        return self.response.transform()
+
+    def delete_pr_review_requests(
+        self, owner, repo, pull_number, reviewers, team_reviewers
+    ):
+        url = f"/repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers"
+        params = {"reviewers": reviewers, "team_reviwers": team_reviewers}
+        self.response = Response(self.delete(url, params=params), "")
+        return self.response.status_code == 204
