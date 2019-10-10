77f53d5334fc21356770b4caa8551b75b93cb8a4
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-10 10:50:29 +0530

    Add methods for Trees API

---
+++ allhub/git_data/trees.py
@@ -0,0 +1,20 @@
+from allhub.response import Response
+
+
+class TreesMixin:
+    def git_tree(self, repo, owner, tree_sha):
+        url = f"/repos/{owner}/{repo}/git/trees/{tree_sha}"
+        self.response = Response(self.get(url), "GitTree")
+        return self.response.transform()
+
+    def git_tree_recursively(self, repo, owner, tree_sha):
+        url = f"/repos/{owner}/{repo}/git/trees/{tree_sha}"
+        self.response = Response(self.get(url, params={"recursive": 1}), "GitTree")
+        return self.response.transform()
+
+    def create_git_tree(self, repo, owner, tree, base_tree):
+        assert isinstance(tree, (list, tuple))
+        url = f"/repos/{owner}/{repo}/git/trees/"
+        params = {"tree": list(tree), "base_tree": base_tree}
+        self.response = Response(self.post(url, params=params), "GitTree")
+        return self.response.transform()
