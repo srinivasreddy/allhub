4059c1db739ae868be44e3ac6e3d21ce7e82bf46
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-10 10:29:38 +0530

    Add methods for references API

---
+++ allhub/git_data/references.py
@@ -0,0 +1,30 @@
+from allhub.response import Response
+
+
+class ReferencesMixin:
+    def reference(self, owner, repo, ref):
+        url = f"/repos/{owner}/{repo}/git/ref/{ref}"
+        self.response = Response(self.get(url), "Reference")
+        return self.response.transform()
+
+    def matching_references(self, owner, repo, ref):
+        url = f"/repos/{owner}/{repo}/git/matching-refs/{ref}"
+        self.response = Response(self.get(url), "References")
+        return self.response.transform()
+
+    def create_reference(self, owner, repo, ref, sha):
+        url = f"/repos/{owner}/{repo}/git/refs"
+        params = {"ref": ref, "sha": sha}
+        self.response = Response(self.post(url, params=params), "Reference")
+        return self.response.transform()
+
+    def update_reference(self, owner, repo, ref, sha, force=False):
+        url = f"/repos/{owner}/{repo}/git/refs/{ref}"
+        params = {"sha": sha, "force": force}
+        self.response = Response(self.patch(url, params=params), "Reference")
+        return self.response.transform()
+
+    def delete_reference(self, owner, repo, ref):
+        url = f"/repos/{owner}/{repo}/git/refs/{ref}"
+        self.response = Response(self.delete(url), "")
+        return self.response.status_code == 204
