47c909361af34c7e6b95bf6e4211490890a28a81
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-11 12:56:20 +0530

    Add **kwargs to misc folder methods.

--- allhub/misc/rate_limit.py
+++ allhub/misc/rate_limit.py
@@ -2,7 +2,7 @@ from allhub.response import Response
 
 
 class RateLimitMixin:
-    def rate_limit(self):
+    def rate_limit(self, **kwargs):
         url = "/rate_limit"
-        self.response = Response(self.get(url), "RateLimit")
+        self.response = Response(self.get(url, **kwargs), "RateLimit")
         return self.response.transform()
