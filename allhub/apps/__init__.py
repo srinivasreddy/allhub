30210daa0449f97067888e2a5d5df99fc3405bba
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-11 12:41:47 +0530

    Reorder imports

--- allhub/apps/__init__.py
+++ allhub/apps/__init__.py
@@ -1,7 +1,8 @@
 # flake8: NOQA
-from .app import *
-from .events import *
-from .permission import *
+from .app import AppMixin
+
+# from .events import *
+from .permission import AppPermission
 from allhub.util import ConflictCheck
 from allhub.apps.installations import InstallationMixin
 
