374d197dd1cc35da69f593b8913d3eaa9db86aa5
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-12 18:14:53 +0530

    Add Python3.5 identifier

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     install_requires=["requests==2.22.0"],
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
