da4232320a3a44f0caf682e48313abf9f84ee7f5
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-12 17:53:55 +0530

    Add org

--- tests/orgs/test_org.py
+++ tests/orgs/test_org.py
@@ -4,11 +4,15 @@ from tests.utils import allhub
 class TestOrg:
     def test_orgs(self):
         response = allhub.organizations()
-        assert response == []
+        assert len(response) == 1
+
+    def test_user_org(self):
+        response = allhub.get_organization("test-github43")
+        response.login == "test-github43"
 
     def test_user_orgs(self):
-        response = allhub.user_organizations("serhiy-storchaka")
-        assert response[0].login == "python"
+        response = allhub.user_organizations("srinivasreddy")
+        assert len(response) == 1
 
     def test_all_orgs(self):
         response = allhub.all_organizations()
@@ -30,8 +34,7 @@ class TestOrg:
         ) == set(response[0].keys())
         counter = 10
         for resp in allhub.iterator(allhub.all_organizations):
-            print(allhub.page)
             if counter == allhub.page:
                 break
         else:
-            assert False, "There are no 10 page ogs"
+            assert False, "There are no 10 orgs in Github"
