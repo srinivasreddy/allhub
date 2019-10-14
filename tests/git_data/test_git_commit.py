1e175633a9bf673ad78f016f223bb7c27fbc1c7f
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-14 17:58:38 +0530

    Create commit failing now.

--- tests/git_data/test_git_commit.py
+++ tests/git_data/test_git_commit.py
@@ -1,4 +1,5 @@
 from tests.utils import allhub
+from datetime import datetime, timezone
 
 
 class TestGitCommit:
@@ -9,4 +10,32 @@ class TestGitCommit:
         assert response.commit.author.name == "Adorilson Bezerra"
 
     def test_create_git_commit(self):
-        pass
+        commits = allhub.commits("test-github42", "cpython", per_page=1)
+        commit = commits[0]
+        message = "First commit!!!"
+        tree = commit.commit.tree.sha
+        parents = [parent.sha for parent in commit.parents]
+        author = {
+            "name": "Srinivas Reddy Thatiparthy",
+            "email": "sr.thatiparthy@gmail.com",
+            "date": datetime.utcnow().replace(tzinfo=timezone.utc).isoformat(),
+        }
+        committer = {
+            "name": "Srinivas Reddy Thatiparthy",
+            "email": "sr.thatiparthy@gmail.com",
+            "date": datetime.utcnow().replace(tzinfo=timezone.utc).isoformat(),
+        }
+        signature = ""
+
+        response = allhub.create_git_commit(
+            "test-github42",
+            "hello-world",
+            message,
+            tree,
+            parents,
+            author,
+            committer,
+            signature,
+        )
+        assert response is not None
+        assert allhub.response.status_code == 201
