2989ed48c1e2b1d4b7b9717acdc277d3cf23a3f6
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-14 15:41:21 +0530

    Move _mime variable out of method to module level

--- allhub/issues/events.py
+++ allhub/issues/events.py
@@ -1,15 +1,16 @@
 from allhub.response import Response
 
+_mime = ", ".join(
+    [
+        "application/vnd.github.starfox-preview+json",
+        "application/vnd.github.symmetra-preview+json",
+        "application/vnd.github.sailor-v-preview+json",
+    ]
+)
+
 
 class IssueEventsMixin:
     def events_for_issue(self, owner, repo, issue_number):
-        _mime = ", ".join(
-            [
-                "application/vnd.github.starfox-preview+json",
-                "application/vnd.github.symmetra-preview+json",
-                "application/vnd.github.sailor-v-preview+json",
-            ]
-        )
         url = "/repos/{owner}/{repo}/issues/{issue_number}/events".format(
             owner=owner, repo=repo, issue_number=issue_number
         )
@@ -17,28 +18,14 @@ class IssueEventsMixin:
         return self.response.transform()
 
     def events_for_repo(self, owner, repo):
-        _mime = ", ".join(
-            [
-                "application/vnd.github.starfox-preview+json",
-                "application/vnd.github.symmetra-preview+json",
-                "application/vnd.github.sailor-v-preview+json",
-            ]
-        )
         url = "/repos/{owner}/{repo}/issues/events".format(owner=owner, repo=repo)
         self.response = Response(self.get(url, **{"Accept": _mime}), "Events")
         return self.response.transform()
 
     def event(self, owner, repo, event_id):
-        _mime = ", ".join(
-            [
-                "application/vnd.github.starfox-preview+json",
-                "application/vnd.github.machine-man-preview",
-                "application/vnd.github.symmetra-preview+json",
-                "application/vnd.github.sailor-v-preview+json",
-            ]
-        )
+        _local_mime = _mime + ", application/vnd.github.machine-man-preview"
         url = "/repos/{owner}/{repo}/issues/events/{event_id}".format(
             owner=owner, repo=repo, event_id=event_id
         )
-        self.response = Response(self.get(url, **{"Accept": _mime}), "Event")
+        self.response = Response(self.get(url, **{"Accept": _local_mime}), "Event")
         return self.response.transform()
