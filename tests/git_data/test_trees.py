ed9280790b766af97c586b5f68627aded2bd29e2
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-15 16:32:11 +0530

    These tests are useless now; but commit anyway

--- tests/git_data/test_trees.py
+++ tests/git_data/test_trees.py
@@ -3,11 +3,19 @@ from tests.utils import allhub
 
 class TestTrees:
     def test_get_tree(self):
-        response = allhub.git_tree("python", "cpython", "3.8")
+        commit = allhub.commits("test-github42", "cpython", per_page=1)[0]
+        tree_sha = commit.commit.tree.sha
+        response = allhub.git_tree("test-github42", "cpython", tree_sha)
         assert response is not None
 
     def test_get_tree_recursively(self):
         pass
 
     def test_create_tree(self):
-        pass
+        commit = allhub.commits("test-github42", "cpython", per_page=1)[0]
+        base_tree = commit.commit.tree.sha
+        tree = [
+            {"path": "aclocal.m4", "mode": "100644", "type": "blob", "sha": commit.sha}
+        ]
+        response = allhub.create_git_tree("test-github42", "cpython", tree, base_tree)
+        assert response is not None
