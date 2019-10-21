b08cca400a7b0e86070954dd441cdb51e1519a53
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-10-21 16:56:02 +0530

    Refactor the changes

--- allhub/repos/collabs.py
+++ allhub/repos/collabs.py
@@ -1,7 +1,7 @@
 from allhub.response import Response
 from enum import Enum
 
-_mime = "application/vnd.github.hellcat-preview+json"
+_mime = {"Accept": "application/vnd.github.hellcat-preview+json"}
 
 
 class CollabPermission(Enum):
@@ -14,8 +14,7 @@ class CollaboratorsMixin:
     def repo_collaborators(self, owner, repo, affiliation="all"):
         url = "/repos/{owner}/{repo}/collaborators".format(owner=owner, repo=repo)
         self.response = Response(
-            self.get(url, params={"affiliation": affiliation}, **{"Accept": _mime}),
-            "Collaborators",
+            self.get(url, params={"affiliation": affiliation}, **_mime), "Collaborators"
         )
         return self.response.transform()
 
@@ -23,14 +22,14 @@ class CollaboratorsMixin:
         url = "/repos/{owner}/{repo}/collaborators/{username}".format(
             owner=owner, repo=repo, username=username
         )
-        self.response = Response(self.get(url, **{"Accept": _mime}), "")
+        self.response = Response(self.get(url, **_mime), "")
         return self.response.status_code == 204
 
     def user_permission(self, owner, repo, username):
         url = "/repos/{owner}/{repo}/collaborators/{username}/permission".format(
             owner=owner, repo=repo, username=username
         )
-        self.response = Response(self.get(url, **{"Accept": _mime}), "")
+        self.response = Response(self.get(url, **_mime), "")
         return self.response.transform()
 
     def add_user_as_collaborator(
@@ -40,8 +39,7 @@ class CollaboratorsMixin:
             owner=owner, repo=repo, username=username
         )
         self.response = Response(
-            self.put(url, params={"permission": permission.value}, **{"Accept": _mime}),
-            "User",
+            self.put(url, params={"permission": permission.value}, **_mime), "User"
         )
         return self.response.transform()
 
@@ -49,5 +47,5 @@ class CollaboratorsMixin:
         url = "/repos/{owner}/{repo}/collaborators/{username}".format(
             owner=owner, repo=repo, username=username
         )
-        self.response = Response(self.delete(url, **{"Accept": _mime}), "User")
+        self.response = Response(self.delete(url, **_mime), "User")
         return self.response.status_code == 204
